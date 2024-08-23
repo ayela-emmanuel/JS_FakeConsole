be1e073b77d965501dc2073d5d3ba28c90d9c9f2
lilwizzi9 <lilwizzi9@gmail.com>
2024-08-23 11:51:17 +0100

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,16 @@
 
+var args = [];
+
 var commands = {
     "Version": ()=>{
         Log("The Current Version is 0.0.1")
+    },
+    "Clear": Clear,
+    "Loop": ()=>{
+        
+        for (let index = 0; index < parseInt(args[0]); index++) {
+            Log("Itration - "+index)
+        }
     }
     
 }
@@ -16,11 +25,17 @@ function ExecuteCommand(){
             LogError("Invalid Command")
         }else{
             Log("$AITECH-TERMINAL~ "+command_input.value)
-            let action = commands[command_input.value];
+            
+            let action = commands[command_input.value.split(" ")[0]];
             if(action){
+                // Fetch Args
+                var input = command_input.value.split(" ");
+                input.shift()
+                args  = input;
+                // Call Command
                 action();
             }else{
-                LogError("Error: Command Not Found!")
+                LogError("Error: Command Not Found! "+action)
                 LogHint("You can look at The Hints Panel To See The List of Commands You can Run :) ")
             }
             ResetInput();
@@ -36,6 +51,9 @@ function ExecuteCommand(){
 
 function Log(data){
     output.innerHTML += "<br>" +data
+}
+function Clear(){
+    output.innerHTML = ""
 }
 function ResetInput(){
     command_input.value = ""
